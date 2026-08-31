e71a51506d7e6778872ed7e0804713800c465bf5
slemanjomaa2006-lgtm <slemanjomaa2006@gmail.com>
2026-08-31 03:27:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,12 +38,14 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda m: m.text == "🌤️ طقس اللاذقية مباشر")
 def get_weather(message):
-    r = requests.get("https://wttr.in", timeout=10).json()
-        temp_air = int(r['current_condition'][0]['temp_C'])
-    except: temp_air = 22
-        temp_surface = temp_air - 2
+   try:
+        r = requests.get("https://wttr.in", timeout=10).json()
+           temp_air = int(r['current_condition'][0]['temp_C'])
+    except:
+        temp_air = 22
+    temp_surface = temp_air - 2
     report = f"📍 طقس محافظة اللاذقية الحصري:\n━━━━━━━━━━━━━━━━━━\n🌡️ درجة حرارة الجو الحالية: {temp_air}°C\n🌍 حرارة السطح الحية (المولّدة): {temp_surface}°C\n🛡️ _وضع الحماية السحابي نشط._"
-        bot.reply_to(message, report, parse_mode="Markdown")
+    bot.reply_to(message, report, parse_mode="Markdown")
         @bot.message_handler(func=lambda m: m.text == "💵 سعر الدولار")
 def (get_dollar_retes(message):
     waitingmsg = bot.reply_to(message, "⚡ جاري قراءة أسعار الدولار الحية من موقع الليرة اليوم...")
